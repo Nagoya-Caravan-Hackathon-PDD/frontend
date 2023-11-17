cb4780257afb429588726fe7f855dcd8cf02d48e
mitsuYashi <hm20011027@gmail.com>
2023-11-17 14:55:55 +0900

    change: icon

--- src/app/game/_components/Game/Game.tsx
+++ src/app/game/_components/Game/Game.tsx
@@ -1,3 +1,4 @@
+import { IconDoorEnter, IconUsers } from '@tabler/icons-react';
 import { Button } from '@/shared/components/common/Button';
 import { Stack } from '@/shared/components/common/Layout';
 import { Route } from '@/shared/types/Page';
@@ -5,10 +6,18 @@ import { Route } from '@/shared/types/Page';
 export const Game = () => {
   return (
     <Stack>
-      <Button component="a" href={Route.createGameRoom}>
+      <Button
+        component="a"
+        href={Route.createGameRoom}
+        leftSection={<IconUsers />}
+      >
         {'Create a game'}
       </Button>
-      <Button component="a" href={Route.joinGameRoom}>
+      <Button
+        component="a"
+        href={Route.joinGameRoom}
+        leftSection={<IconDoorEnter />}
+      >
         {'Join the game'}
       </Button>
     </Stack>

67bd26ffbf35d9facda3ad85697ab70dea4e9540
yudai2929 <yudai201729@icloud.com>
2023-11-18 05:40:30 +0900

    feat: join room

--- src/app/game/create/_components/CreateGame.tsx
+++ src/app/game/create/_components/CreateGame.tsx
@@ -1,25 +1,40 @@
 'use client';
 import { Button, Stack } from '@mantine/core';
 
-import { useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { QRCard } from '@/shared/components/features/QRCard/QRCard';
 import { Heading } from '@/shared/components/layout';
+import { useReadyGame } from '@/shared/hooks/restapi/game/id/ready/post';
+import { useCreateGame } from '@/shared/hooks/restapi/game/post';
 import { theme } from '@/shared/lib/mantine/theme';
 import { Route } from '@/shared/types/Page';
 
 export const CreateGame = () => {
   // TODO: サーバーからIDを受け取る
-  const id = '1234567890';
-  const url = process.env.NEXT_PUBLIC_BASE_URL + '/' + Route.game + '/' + id;
+  const { createGame } = useCreateGame();
+  const { readyGame } = useReadyGame();
+  const [gameId, setGameId] = useState<string>();
+  const [gameServerToken, setGameServerToken] = useState<string>();
+  const url = useMemo(
+    () => process.env.NEXT_PUBLIC_BASE_URL + '/' + Route.game + '/' + gameId,
+    [gameId],
+  );
+
   const [isReady, setIsReady] = useState(false);
-  const handleClickReady = () => {
-    if (isReady) {
-      // TODO: サーバーに準備完了を伝える
-    } else {
-      // TODO: サーバーに未準備完了を伝える
-    }
-    setIsReady((prev) => !prev);
-  };
+
+  const handleClickReady = useCallback(async () => {
+    if (!gameId || !gameServerToken) return;
+    await readyGame(gameId);
+    setIsReady(true);
+  }, [gameId, gameServerToken, readyGame]);
+
+  useEffect(() => {
+    (async () => {
+      const result = await createGame();
+      setGameId(result?.gameId);
+      setGameServerToken(result?.gameServerToken);
+    })();
+  }, [createGame]);
 
   return (
     <Heading title="Real time battle">
--- src/app/game/join/_components/Join.tsx
+++ src/app/game/join/_components/Join.tsx
@@ -1,11 +1,23 @@
 'use client';
+import { useRouter } from 'next/router';
 import { JoinGameURLForm } from './JoinGameURLForm';
 import { QRCodeReader } from '@/shared/components/features/QRCode/QRCodeReader';
+import { useJoinGame } from '@/shared/hooks/restapi/game/id/post';
+import { Route } from '@/shared/types/Page';
 
 export const Join = () => {
+  const { joinGame } = useJoinGame();
+  const router = useRouter();
+  const handleSubmit = async (url: string) => {
+    const gameId = url.split('/').pop();
+    if (!gameId) return;
+    const result = await joinGame(gameId);
+    if (!result) return;
+    router.push(Route.game + gameId);
+  };
   return (
     <>
-      <JoinGameURLForm />
+      <JoinGameURLForm onSubmit={handleSubmit} />
       <QRCodeReader />
     </>
   );

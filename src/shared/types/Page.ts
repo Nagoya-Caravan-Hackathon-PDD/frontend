b7d3af93e19c95e8a3d26553b8c599588a7d5520
hkiku482 <h.kikuchi482@gmail.com>
2023-11-17 04:20:25 +0000

    feat: pages

--- src/shared/types/Page.ts
+++ src/shared/types/Page.ts
@@ -1,6 +1,16 @@
-type Page = 'root' | 'signOut';
+type Page =
+  | 'root'
+  | 'crossingHistory'
+  | 'game'
+  | 'profile'
+  | 'signOut'
+  | 'result';
 
 export const Route = {
   root: '/',
-  signOut: 'sign_out/',
+  crossingHistory: 'crossing_history',
+  game: 'game',
+  profile: 'profile',
+  signOut: 'sign_out',
+  result: 'result',
 } as const satisfies Record<Page, string>;

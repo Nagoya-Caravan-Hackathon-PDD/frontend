9b55b46ee2140a732bf87caa440548129d76d4ff
yudai2929 <yudai201729@icloud.com>
2023-11-17 22:29:59 +0900

    👍fix: 準備完了処理を追加

--- src/app/game/create/_components/CreateGame.tsx
+++ src/app/game/create/_components/CreateGame.tsx
@@ -1,19 +1,36 @@
+'use client';
 import { Button, Stack } from '@mantine/core';
 
+import { useState } from 'react';
 import { QRCard } from '@/shared/components/features/QRCard/QRCard';
 import { Heading } from '@/shared/components/layout';
+import { theme } from '@/shared/lib/mantine/theme';
 import { Route } from '@/shared/types/Page';
 
 export const CreateGame = () => {
-  // NOTE: サーバーからIDを受け取る
+  // TODO: サーバーからIDを受け取る
   const id = '1234567890';
   const url = process.env.NEXT_PUBLIC_BASE_URL + '/' + Route.game + '/' + id;
-  const isReady = false;
+  const [isReady, setIsReady] = useState(false);
+  const handleClickReady = () => {
+    if (isReady) {
+      // TODO: サーバーに準備完了を伝える
+    } else {
+      // TODO: サーバーに未準備完了を伝える
+    }
+    setIsReady((prev) => !prev);
+  };
+
   return (
     <Heading title="Real time battle">
       <Stack>
         <QRCard url={url} />
-        <Button>Ready to go</Button>
+        <Button
+          color={isReady ? 'red' : theme.primaryColor}
+          onClick={handleClickReady}
+        >
+          Ready to go
+        </Button>
       </Stack>
     </Heading>
   );

db3ee52c124b077f56fc1ed16633ac305e33a3ba
yudai2929 <yudai201729@icloud.com>
2023-11-17 22:17:54 +0900

    👍fix: env var name

--- src/app/game/create/_components/CreateGame.tsx
+++ src/app/game/create/_components/CreateGame.tsx
@@ -7,7 +7,7 @@ import { Route } from '@/shared/types/Page';
 export const CreateGame = () => {
   // NOTE: サーバーからIDを受け取る
   const id = '1234567890';
-  const url = process.env.NEXT_PUBLIC_API_ORIGIN + '/' + Route.game + '/' + id;
+  const url = process.env.NEXT_PUBLIC_BASE_URL + '/' + Route.game + '/' + id;
   const isReady = false;
   return (
     <Heading title="Real time battle">

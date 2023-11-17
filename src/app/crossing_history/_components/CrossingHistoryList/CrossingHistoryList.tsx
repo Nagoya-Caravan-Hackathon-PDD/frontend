217fe1b0b4bf926901be92922c36e4d6342a97d6
hkiku482 <h.kikuchi482@gmail.com>
2023-11-17 12:05:22 +0000

    wip: crossing histories

---
+++ src/app/crossing_history/_components/CrossingHistoryList/CrossingHistoryList.tsx
@@ -0,0 +1,10 @@
+import { List } from '@/shared/components/common/List';
+
+type Props = {};
+export const CrossingHistoryList = ({}: Props) => {
+  return (
+    <List>
+      <List.Item></List.Item>
+    </List>
+  );
+};
